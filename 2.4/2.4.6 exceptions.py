ed4f7774ea64047b87612e01434da75302ef53cd
Тимофей Трунов <timofejtrunov@Timofejs-MacBook-Pro.local>
2021-12-07 21:50:56 +0300

    Changed 2.4.6 file a little bit

--- 2.4/2.4.6 exceptions.py
+++ 2.4/2.4.6 exceptions.py
@@ -3,8 +3,7 @@ from selenium import webdriver
 
 url = "http://suninjuly.github.io/cats.html"
 
-with webdriver.Chrome() as browser:
-    browser.implicitly_wait(5)
-    browser.get(url)
-
-    browser.find_element_by_id("button")
+#with webdriver.Chrome() as browser:
+#    browser.implicitly_wait(5)
+#    browser.get(url)
+#    browser.find_element_by_id("button")
